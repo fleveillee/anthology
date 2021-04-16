f30f88520456e7d35ad273a616fd600be575b6d5
Frédéric Léveillée <fleveillee@gmail.com>
2021-04-15 23:43:51 -0400

    Enable JPA Auditing

--- src/main/java/eiffelis/anthology/AnthologyApplication.java
+++ src/main/java/eiffelis/anthology/AnthologyApplication.java
@@ -2,12 +2,14 @@ package eiffelis.anthology;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // Used to enable @CreatedDate and @LastModifiedDate fields in BaseEntity
 public class AnthologyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AnthologyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AnthologyApplication.class, args);
+    }
 
 }
